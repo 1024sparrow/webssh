6342bdc6cd8c405f80ad042749db5811360a5a10
1024sparrow <1024sparrow@gmail.com>
2020-05-26 05:55:58 +0300

    screen_keyboard: buttons layouted. Not working on mobile yet (only desktop - oops...).

--- webssh/static/js/keyboard.js
+++ webssh/static/js/keyboard.js
@@ -12,137 +12,162 @@ function generate_keyboard(p_eContainer){
 		height: 18,
 		rows: [ // rows
 			{
-				size: 2,
+				height: 2,
 				buttons: [
 					// button: if id not set, use "text" field value instead
-					{ size: 3, subitems: [ { text: "Esc" } ] },
-					{ size: 3, subitems: [ { text: "F1" } ] },
-					{ size: 3, subitems: [ { text: "F2" } ] },
-					{ size: 3, subitems: [ { text: "F3" } ] },
-					{ size: 3, subitems: [ { text: "F4" } ] },
-					{ size: 3, subitems: [ { text: "F5" } ] },
-					{ size: 3, subitems: [ { text: "F6" } ] },
-					{ size: 3, subitems: [ { text: "F7" } ] },
-					{ size: 3, subitems: [ { text: "F8" } ] },
-					{ size: 3, subitems: [ { text: "F9" } ] },
-					{ size: 3, subitems: [ { text: "F10" } ] },
-					{ size: 3, subitems: [ { text: "F12" } ] },
-					{ size: 3, subitems: [ { text: "F13" } ] },
-					{ size: 3, subitems: [ { text: "PrtSc" } ] },
-					{ size: 3, subitems: [ { text: "Ins" } ] },
-					{ size: 3, subitems: [ { text: "Del" } ] }
+					{ width: 3, subitems: [ { text: "Esc" } ] },
+					{ width: 3, subitems: [ { text: "F1" } ] },
+					{ width: 3, subitems: [ { text: "F2" } ] },
+					{ width: 3, subitems: [ { text: "F3" } ] },
+					{ width: 3, subitems: [ { text: "F4" } ] },
+					{ width: 3, subitems: [ { text: "F5" } ] },
+					{ width: 3, subitems: [ { text: "F6" } ] },
+					{ width: 3, subitems: [ { text: "F7" } ] },
+					{ width: 3, subitems: [ { text: "F8" } ] },
+					{ width: 3, subitems: [ { text: "F9" } ] },
+					{ width: 3, subitems: [ { text: "F10" } ] },
+					{ width: 3, subitems: [ { text: "F12" } ] },
+					{ width: 3, subitems: [ { text: "F13" } ] },
+					{ width: 3, subitems: [ { text: "PrtSc" } ] },
+					{ width: 3, subitems: [ { text: "Ins" } ] },
+					{ width: 3, subitems: [ { text: "Del" } ] }
 				]
 			},
 			{
-				size: 3,
+				height: 3,
 				buttons: [
-					{ size: 3, subitems: [ { id: "`", text: "` ~" } ] },
-					{ size: 3, subitems: [ { id: "1", text: "1 !" } ] },
-					{ size: 3, subitems: [ { id: "2", text: "2 @" } ] },
-					{ size: 3, subitems: [ { id: "3", text: "3 #" } ] },
-					{ size: 3, subitems: [ { id: "4", text: "4 $" } ] },
-					{ size: 3, subitems: [ { id: "5", text: "5 %" } ] },
-					{ size: 3, subitems: [ { id: "6", text: "6 ^" } ] },
-					{ size: 3, subitems: [ { id: "7", text: "7 &" } ] },
-					{ size: 3, subitems: [ { id: "8", text: "8 *" } ] },
-					{ size: 3, subitems: [ { id: "9", text: "9 (" } ] },
-					{ size: 3, subitems: [ { id: "0", text: "0 )" } ] },
-					{ size: 3, subitems: [ { id: "-", text: "- _" } ] },
-					{ size: 3, subitems: [ { id: "=", text: "= +" } ] },
-					{ size: 3, subitems: [ { id: "BS", text: "BS" } ] },
-					{ size: 3, subitems: [ { id: "Home", text: "Home" } ] }
+					{ width: 3, subitems: [ { id: "`", text: "` ~" } ] },
+					{ width: 3, subitems: [ { id: "1", text: "1 !" } ] },
+					{ width: 3, subitems: [ { id: "2", text: "2 @" } ] },
+					{ width: 3, subitems: [ { id: "3", text: "3 #" } ] },
+					{ width: 3, subitems: [ { id: "4", text: "4 $" } ] },
+					{ width: 3, subitems: [ { id: "5", text: "5 %" } ] },
+					{ width: 3, subitems: [ { id: "6", text: "6 ^" } ] },
+					{ width: 3, subitems: [ { id: "7", text: "7 &" } ] },
+					{ width: 3, subitems: [ { id: "8", text: "8 *" } ] },
+					{ width: 3, subitems: [ { id: "9", text: "9 (" } ] },
+					{ width: 3, subitems: [ { id: "0", text: "0 )" } ] },
+					{ width: 3, subitems: [ { id: "-", text: "- _" } ] },
+					{ width: 3, subitems: [ { id: "=", text: "= +" } ] },
+					{ width: 6, subitems: [ { id: "BS", text: "BS" } ] },
+					{ width: 3, subitems: [ { id: "Home", text: "Home" } ] }
 				]
 			},
 			{
-				size: 3,
+				height: 3,
 				buttons: [
-					{ size: 4, subitems: [ { text: "Tab" } ] },
-					{ size: 3, subitems: [ { text: "Q" } ] },
-					{ size: 3, subitems: [ { text: "W" } ] },
-					{ size: 3, subitems: [ { text: "E" } ] },
-					{ size: 3, subitems: [ { text: "R" } ] },
-					{ size: 3, subitems: [ { text: "T" } ] },
-					{ size: 3, subitems: [ { text: "Y" } ] },
-					{ size: 3, subitems: [ { text: "U" } ] },
-					{ size: 3, subitems: [ { text: "I" } ] },
-					{ size: 3, subitems: [ { text: "O" } ] },
-					{ size: 3, subitems: [ { text: "P" } ] },
-					{ size: 3, subitems: [ { text: "[ {" } ] },
-					{ size: 3, subitems: [ { text: "] }" } ] },
-					{ size: 5, subitems: [ { text: "\\ |" } ] },
-					{ size: 3, subitems: [ { text: "PgUp" } ] }
+					{ width: 4, subitems: [ { text: "Tab" } ] },
+					{ width: 3, subitems: [ { text: "Q" } ] },
+					{ width: 3, subitems: [ { text: "W" } ] },
+					{ width: 3, subitems: [ { text: "E" } ] },
+					{ width: 3, subitems: [ { text: "R" } ] },
+					{ width: 3, subitems: [ { text: "T" } ] },
+					{ width: 3, subitems: [ { text: "Y" } ] },
+					{ width: 3, subitems: [ { text: "U" } ] },
+					{ width: 3, subitems: [ { text: "I" } ] },
+					{ width: 3, subitems: [ { text: "O" } ] },
+					{ width: 3, subitems: [ { text: "P" } ] },
+					{ width: 3, subitems: [ { text: "[ {" } ] },
+					{ width: 3, subitems: [ { text: "] }" } ] },
+					{ width: 5, subitems: [ { text: "\\ |" } ] },
+					{ width: 3, subitems: [ { text: "PgUp" } ] }
 				]
 			},
 			{
-				size: 3,
+				height: 3,
 				buttons: [
-					{ size: 5, subitems: [ { text: "Caps" } ] },
-					{ size: 3, subitems: [ { text: "A" } ] },
-					{ size: 3, subitems: [ { text: "S" } ] },
-					{ size: 3, subitems: [ { text: "D" } ] },
-					{ size: 3, subitems: [ { text: "F" } ] },
-					{ size: 3, subitems: [ { text: "G" } ] },
-					{ size: 3, subitems: [ { text: "H" } ] },
-					{ size: 3, subitems: [ { text: "J" } ] },
-					{ size: 3, subitems: [ { text: "K" } ] },
-					{ size: 3, subitems: [ { text: "L" } ] },
-					{ size: 3, subitems: [ { text: "; :" } ] },
-					{ size: 3, subitems: [ { text: "' \"" } ] },
-					{ size: 7, subitems: [ { text: "Ent" } ] },
-					{ size: 3, subitems: [ { text: "PgDn" } ] }
+					{ width: 5, subitems: [ { text: "Caps" } ] },
+					{ width: 3, subitems: [ { text: "A" } ] },
+					{ width: 3, subitems: [ { text: "S" } ] },
+					{ width: 3, subitems: [ { text: "D" } ] },
+					{ width: 3, subitems: [ { text: "F" } ] },
+					{ width: 3, subitems: [ { text: "G" } ] },
+					{ width: 3, subitems: [ { text: "H" } ] },
+					{ width: 3, subitems: [ { text: "J" } ] },
+					{ width: 3, subitems: [ { text: "K" } ] },
+					{ width: 3, subitems: [ { text: "L" } ] },
+					{ width: 3, subitems: [ { text: "; :" } ] },
+					{ width: 3, subitems: [ { text: "' \"" } ] },
+					{ width: 7, subitems: [ { text: "Ent" } ] },
+					{ width: 3, subitems: [ { text: "PgDn" } ] }
 				]
 			},
 			{
-				size: 3,
+				height: 3,
 				buttons: [
-					{ size: 6, subitems: [ { text: "Sh" } ] },
-					{ size: 3, subitems: [ { text: "Z" } ] },
-					{ size: 3, subitems: [ { text: "X" } ] },
-					{ size: 3, subitems: [ { text: "C" } ] },
-					{ size: 3, subitems: [ { text: "V" } ] },
-					{ size: 3, subitems: [ { text: "B" } ] },
-					{ size: 3, subitems: [ { text: "N" } ] },
-					{ size: 3, subitems: [ { text: "M" } ] },
-					{ size: 3, subitems: [ { text: ", <" } ] },
-					{ size: 3, subitems: [ { text: ". >" } ] },
-					{ size: 3, subitems: [ { text: "/ ?" } ] },
-					{ size: 9, subitems: [ { text: "Sh" } ] },
-					{ size: 3, subitems: [ { text: "End" } ] }
+					{ width: 6, subitems: [ { text: "Sh" } ] },
+					{ width: 3, subitems: [ { text: "Z" } ] },
+					{ width: 3, subitems: [ { text: "X" } ] },
+					{ width: 3, subitems: [ { text: "C" } ] },
+					{ width: 3, subitems: [ { text: "V" } ] },
+					{ width: 3, subitems: [ { text: "B" } ] },
+					{ width: 3, subitems: [ { text: "N" } ] },
+					{ width: 3, subitems: [ { text: "M" } ] },
+					{ width: 3, subitems: [ { text: ", <" } ] },
+					{ width: 3, subitems: [ { text: ". >" } ] },
+					{ width: 3, subitems: [ { text: "/ ?" } ] },
+					{ width: 9, subitems: [ { text: "Sh" } ] },
+					{ width: 3, subitems: [ { text: "End" } ] }
 				]
 			},
 			{
-				size: 4,
+				height: 4,
 				buttons: [
-					{ size: 4, subitems: [ { text: "CtrlL" } ] },
-					{ size: 4, subitems: [ { text: "WinL" } ] },
-					{ size: 3, subitems: [ { text: "AltL" } ] },
-					{ size: 3, subitems: [ { text: "Space" } ] },
-					{ size: 3, subitems: [ { text: "AltR" } ] },
-					{ size: 3, subitems: [ { text: "WinR" } ] },
-					{ size: 3, subitems: [ { text: "CtrlR" } ] },
-					{ size: 3, subitems: [ { text: "<-" } ] },
-					{ size: 3, subitems: [ { text: "Up" }, { text: "Down" } ] },
-					{ size: 3, subitems: [ { text: "->" } ] }
+					{ width: 4, subitems: [ { text: "CtrlL" } ] },
+					{ width: 4, subitems: [ { text: "WinL" } ] },
+					{ width: 4, subitems: [ { text: "AltL" } ] },
+					{ width: 16, subitems: [ { text: "Space" } ] },
+					{ width: 4, subitems: [ { text: "AltR" } ] },
+					{ width: 4, subitems: [ { text: "WinR" } ] },
+					{ width: 3, subitems: [ { text: "CtrlR" } ] },
+					{ width: 3, subitems: [ { text: "<-" } ] },
+					{ width: 3, subitems: [ { text: "Up" }, { text: "Down" } ] },
+					{ width: 3, subitems: [ { text: "->" } ] }
 				]
 			}
 		] // rows
 	};
 
-	var bn = document.createElement('div');
-	bn.style.position = 'absolute';
-	bn.style.color = 'green';
-	bn.style.top = '50px';
-	bn.style.left = '50px';
-	bn.style.width = '250px';
-	bn.style.height = '250px';
-	bn.innerHTML = 'QQ';
-
-	p_eContainer.appendChild(bn);
-    bn.addEventListener('click', generateKeyEvent);
     /*bn.addEventListener('click', function($self){return function(){
         //$self.$_onClicked();
 		generateKeyEvent();
     };}(this));*/
+
+	var bn;
+	const heightKoef = window.innerWidth / layout.height;
+	const widthKoef = innerHeight / layout.width;
+	var xCell, yRow = 0, yCell, wCell, hRow, hCell;
+	for (const oRow of layout.rows){
+		xCell = 0;
+		hRow = heightKoef * oRow.height;
+		for (const oButton of oRow.buttons){
+			wCell = widthKoef * oButton.width;
+
+			if (!oButton.subitems.length)
+			{
+				console.error("incorrect empty set of buttons in columns");
+				return;
+			}
+			hCell = hRow / oButton.subitems.length;
+			yCell = yRow;
+			for (const oSubitem of oButton.subitems){
+				// create button here
+				bn = document.createElement('div');
+				bn.innerHTML = oSubitem.text;
+				bn.className = 'keyboard__key';
+				bn.style.top = `${yCell}px`;
+				bn.style.left = `${xCell}px`;
+				bn.style.width = `${wCell}px`;
+				bn.style.height = `${hCell}px`;
+				p_eContainer.appendChild(bn);
+				bn.addEventListener('click', generateKeyEvent);
+
+				yCell += hCell;
+			}
+			xCell += wCell;
+		}
+		yRow += hRow;
+	}
 
 }; // function generate_keyboard
 
