fc63997ddc02239f847c758ec8268d7bcb50066b
1024sparrow <1024sparrow@gmail.com>
2020-06-27 21:29:40 +0300

    screen_keyboard

---
+++ webssh/static/skb_languages/skb_languages.js
@@ -0,0 +1,19 @@
+ScreenKeyboardLanguages = [
+	{
+		id: 'russian',
+		buttons: {
+			q: { text: "q", keycode: { normal: [ 113 ], shift: [ 81 ], alt: [ 27,113 ], ctrl: [ 17 ] } },
+			w: { text: "w", keycode: { normal: [ 119 ], shift: [ 87 ], alt: [ 27,119 ], ctrl: [ 23 ] } },
+			e: { text: "e", keycode: { normal: [ 101 ], shift: [ 69 ], alt: [ 27,101 ], ctrl: [ 5 ] } },
+			r: { text: "r", keycode: { normal: [ 114 ], shift: [ 82 ], alt: [ 27,114 ], ctrl: [ 18 ] } },
+			t: { text: "t", keycode: { normal: [ 116 ], shift: [ 84 ], alt: [ 27,116 ], ctrl: [ 20 ] } },
+			y: { text: "y", keycode: { normal: [ 121 ], shift: [ 89 ], alt: [ 27,121 ], ctrl: [ 25 ] } },
+			u: { text: "u", keycode: { normal: [ 117 ], shift: [ 85 ], alt: [ 27,117 ], ctrl: [ 21 ] } },
+			i: { text: "i", keycode: { normal: [ 105 ], shift: [ 73 ], alt: [ 27,105 ], ctrl: [ 9 ] } },
+			o: { text: "o", keycode: { normal: [ 111 ], shift: [ 79 ], alt: [ 27,111 ], ctrl: [ 15 ] } },
+			p: { text: "p", keycode: { normal: [ 112 ], shift: [ 80 ], alt: [ 27,112 ], ctrl: [ 16 ] } },
+			figstart: { text: "figstart", shift_text: "shift_figstart", keycode: { normal: [ 91 ], shift: [ 123 ], alt: [ 27,91 ], ctrl: [ 27 ] } },
+			figend: { text: "figend", shift_text: "shift_figend", keycode: { normal: [ 93 ], shift: [ 125 ], alt: [ 27,93 ], ctrl: [ 29 ] } }
+		}
+	}
+];
