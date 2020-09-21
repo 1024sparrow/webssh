2bb620b4cf0b5dc3a8c601634fdc3e8420ce86b5
1024sparrow <1024sparrow@gmail.com>
2020-09-21 19:02:43 +0300

    sound

--- webssh/sound.py
+++ webssh/sound.py
@@ -1,7 +1,7 @@
 import threading
 import pipes
+import time
 
-#def 
 
 class Sound:
 	# boris here
@@ -10,7 +10,31 @@ class Sound:
 		# ('boris debug 009181: ', {'use_sound': True, 'capturePipe': 'boris/capture.pipe', 'use_c': True, 'playbackPipe': 'boris/playback.pipe', 'use_p': True})
 		# self._pipe_p = pipe_p
 		# self._pipe_c = pipe_c
-		pass
+		self._tP = Thread(target=self.run_p)
+		self._tC = Thread(target=self.run_c)
+		self._running = false
+
+	def run_stub(self):
+		return
+
+	def run_p(self):
+		return
+
+	def run_c(self):
+		return
+
+	def start(self, p_hostname, p_username):
+		if p_hostname != 'localhost':
+			print('can not start sound for any host but "localhost": not implemented')
+			return
+		self._running = true
+		self._tP.start()
+		self._tC.start()
+
+	def stop(self):
+		self._running = false
+		self._tP.join()
+		self._tC.join()
 
 	# read data from a pipe and get the data to write to web-client
 	def data_to_write(self):
