ad41ac2e69b65318ad6a0b33f27977a7051f528c
1024sparrow <1024sparrow@gmail.com>
2021-02-02 21:29:16 +0300

    123

--- src/sound.py/t.py
+++ src/sound.py/t.py
@@ -7,7 +7,7 @@ import time
 
 class Sound:
 	# boris here
-	def __init__(self, p_sound):
+	def __init__(self, p_loop, p_sound):
 		#open()
 		# ('boris debug 009181: ', {'use_sound': True, 'capturePipe': 'boris/capture.pipe', 'use_c': True, 'playbackPipe': 'boris/playback.pipe', 'use_p': True})
 		# self._pipe_p = pipe_p
